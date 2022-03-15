346993f1b98a22ed9c0df06304cf8b58bb569f72
Uy, Nickolai Cean Cecil <nickolai.cean.cecil.uy@oracle.com>
2022-03-15 18:07:17 +0800

    Java Take Home

---
+++ src/ItemSeparator.java
@@ -0,0 +1,93 @@
+
+public class ItemSeparator {
+	private String delimiter = "\\$\\$##";
+	private String rawInput;
+	private String [] parsedString;
+	private String itemName;
+	private double itemPrice;
+	private int itemQuantity;
+	
+	public ItemSeparator(String rawInput) {
+		this.rawInput = rawInput;
+	}
+	
+	public void parseInput() {
+	
+		if(checkValidInput()){
+			parseRawInput();
+			if (isDouble(parsedString[1]))
+			{
+				if(isInt(parsedString[2]))
+				{
+					itemName = parsedString[0];
+					itemPrice = Double.parseDouble(parsedString[1]);
+					itemQuantity = Integer.parseInt(parsedString[2]);
+				}
+				else {
+					System.out.println(parsedString[2] + " cannot be parsed into type Int.");
+				}
+			}
+			else {
+				System.out.println(parsedString[1] + " cannot be parsed into type Double.");
+			}
+		}
+		else {
+			System.out.println(rawInput + " is an invalid input. Required input format is: ItemName$$##ItemPrice$$##ItemQuantity");
+		}
+		
+	}
+	
+	private boolean checkValidInput() {
+		
+		if (rawInput.split(delimiter).length !=3) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	private void parseRawInput() {
+		
+		parsedString = rawInput.split(delimiter);
+	}
+	
+	private boolean isDouble(String price) 
+	{
+		try 
+		{
+			Double.parseDouble(price);
+			return true;
+		}
+		catch(NumberFormatException e) 
+		{
+			return false;
+		}
+	}
+	
+	private boolean isInt(String quantity) 
+	{
+		try 
+		{
+			Integer.parseInt(quantity);
+			return true;
+		}
+		catch(NumberFormatException e)
+		{
+			return false;
+		}
+	}
+	
+	public String getName() {
+		return itemName;
+	}
+	
+	public double getPrice() {
+		return itemPrice;
+	}
+	
+	public int getQuantity() {
+		return itemQuantity;
+	}
+    
+}
---
+++ src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		   Scanner sc = new Scanner(System.in);
+	        
+		   System.out.println("Enter Input with Format = {\"ItemName$$##ItemPrice$$##ItemQuantity\"}: ");
+		   String sub = sc.nextLine();
+		 
+		   //"ItemName$$##ItemPrice$$##ItemQuantity"//
+	        ItemSeparator itemData = new ItemSeparator(sub);
+	        itemData.parseInput();
+	        
+	        System.out.println("Item Name: " + itemData.getName());
+	        System.out.println("Item Price: " + itemData.getPrice());
+	        System.out.println("Item Quantity: " + itemData.getQuantity());
+		
+	}
+
+}
